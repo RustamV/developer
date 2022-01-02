15a443d0a39e5c7c86043f6e18511504ee24390c
RustamV <rvrd1996@gmail.com>
2022-01-03 00:15:55 +0300

    antd & grid

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,23 @@
+import { Row, Col } from "antd";
+import { Contacts, Education, Experience, Profile, Skills } from "../../components";
+import styles from "./index.module.scss";
+
 const App: React.FC = () => {
-    return <div>123</div>;
+    return (
+        <div className={styles.app}>
+            <Row gutter={30}>
+                <Col xs={16}>
+                    <Profile />
+                    <Experience />
+                    <Education />
+                </Col>
+                <Col xs={8}>
+                    <Contacts />
+                    <Skills />
+                </Col>
+            </Row>
+        </div>
+    );
 };
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export { default as App } from "./App";
+export { default as Contacts } from "./Contacts";
+export { default as Education } from "./Education";
+export { default as Experience } from "./Experience";
+export { default as Profile } from "./Profile";
+export { default as Skills } from "./Skills";
